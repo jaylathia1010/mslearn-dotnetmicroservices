aec4cf82ecd9271da13251988e06c83386a99245
Jay Lathia <jmlathia@gmail.com>
2022-10-03 22:43:59 +0530

    feat: created mocked data

--- backend/Controllers/PizzaInfoController.cs
+++ backend/Controllers/PizzaInfoController.cs
@@ -1,12 +1,8 @@
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-
-using Microsoft.Extensions.Caching.Distributed;
-using System.Text.Json;
+using backend.Models;
+using backend.Data;
 
 namespace backend.Controllers
 {
@@ -14,27 +10,29 @@ namespace backend.Controllers
     [Route("[controller]")]
     public class PizzaInfoController : ControllerBase
     {
-        private static readonly PizzaInfo[] TheMenu = new[]
-        {
-            new PizzaInfo { PizzaName = "The Mighty Meatball", Ingredients = "Meatballs and cheese", Cost = 40, InStock = "yes"},
-            new PizzaInfo { PizzaName = "Crab Apple", Ingredients = "Dungeness crab and apples", Cost = 35, InStock = "no"},
-            new PizzaInfo { PizzaName = "Forest Floor", Ingredients = "Mushrooms, rutabagas, and walnuts", Cost = 20, InStock = "yes"},
-            new PizzaInfo { PizzaName = "Don't At Me", Ingredients = "Pineapple, Canadian bacon, jalapeños", Cost = 25, InStock = "yes"},
-            new PizzaInfo { PizzaName = "Vanilla", Ingredients = "Sausage and pepperoni", Cost = 15, InStock = "no"},
-            new PizzaInfo { PizzaName = "Spice Coming At Ya", Ingredients = "Peppers, chili sauce, spicy andouille", Cost = 50, InStock = "yes"}
-        };
-
+        // private static readonly PizzaInfo[] TheMenu = new[]
+        // {
+        //     new PizzaInfo { PizzaName = "The Mighty Meatball", Ingredients = "Meatballs and cheese", Cost = 40, InStock = "yes"},
+        //     new PizzaInfo { PizzaName = "Crab Apple", Ingredients = "Dungeness crab and apples", Cost = 35, InStock = "no"},
+        //     new PizzaInfo { PizzaName = "Forest Floor", Ingredients = "Mushrooms, rutabagas, and walnuts", Cost = 20, InStock = "yes"},
+        //     new PizzaInfo { PizzaName = "Don't At Me", Ingredients = "Pineapple, Canadian bacon, jalapeños", Cost = 25, InStock = "yes"},
+        //     new PizzaInfo { PizzaName = "Vanilla", Ingredients = "Sausage and pepperoni", Cost = 15, InStock = "no"},
+        //     new PizzaInfo { PizzaName = "Spice Coming At Ya", Ingredients = "Peppers, chili sauce, spicy andouille", Cost = 50, InStock = "yes"}
+        // };
         private readonly ILogger<PizzaInfoController> _logger;
+        private IPizzaRepo _pizzaRepo;
 
-        public PizzaInfoController(ILogger<PizzaInfoController> logger)
+        public PizzaInfoController(ILogger<PizzaInfoController> logger,
+            IPizzaRepo pizzaRepo)
         {
             _logger = logger;
+            _pizzaRepo = pizzaRepo;
         }
 
-         [HttpGet]
-         public IEnumerable<PizzaInfo> Get()
-         {
-             return TheMenu;
-         }
+        [HttpGet]
+        public IEnumerable<PizzaDetail> Get()
+        {
+            return _pizzaRepo.GetAllPizzas();
+        }
     }
 }
---
+++ backend/Data/MockedData.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Linq;
+using backend.Models;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace backend.Data
+{
+    public static class MockedData
+    {
+        public static void PopulateData(IApplicationBuilder app)
+        {
+            using (var serviceScope = app.ApplicationServices.CreateScope())
+            {
+                SeedData(serviceScope.ServiceProvider.GetService<AppDbContext>());
+            }
+        }
+
+        private static void SeedData(AppDbContext context)
+        {
+            if (!context.PizzaDetails.Any())
+            {
+                Console.WriteLine("---> Seeding the Data...!");
+
+                context.PizzaDetails.AddRange(
+                    new PizzaDetail { PizzaName = "French Bread", Ingredients = "Tomato sauce, cheese, pepperoni", Cost = 40, InStock = "yes" },
+                    new PizzaDetail { PizzaName = "Baked Ziti", Ingredients = "Ziti, tomato sauce, ricotta, mozzarella", Cost = 35, InStock = "no" },
+                    new PizzaDetail { PizzaName = "Forest Floor", Ingredients = "Mushrooms, rutabagas, and walnuts", Cost = 20, InStock = "yes" },
+                    new PizzaDetail { PizzaName = "Don't At Me", Ingredients = "Pineapple, Canadian bacon, jalapeños", Cost = 25, InStock = "yes" },
+                    new PizzaDetail { PizzaName = "Vanilla", Ingredients = "Sausage and pepperoni", Cost = 15, InStock = "no" },
+                    new PizzaDetail { PizzaName = "Spice Coming At Ya", Ingredients = "Peppers, chili sauce, spicy andouille", Cost = 50, InStock = "yes" }
+                );
+
+                context.SaveChanges();
+            }
+            else
+            {
+                Console.WriteLine("---> Data already exist...!");
+            }
+        }
+    }
+}
