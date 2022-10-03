d6f2a1541d5972657c804f1ae6e0afe1576e589a
Jay Lathia <jmlathia@gmail.com>
2022-10-04 00:07:00 +0530

    fix: frontend fix for api-controller

--- backend/Controllers/PizzaDetailController.cs
+++ backend/Controllers/PizzaDetailController.cs
@@ -12,15 +12,6 @@ namespace backend.Controllers
     [Route("api/[controller]")]
     public class PizzaDetailController : ControllerBase
     {
-        // private static readonly PizzaInfo[] TheMenu = new[]
-        // {
-        //     new PizzaInfo { PizzaName = "The Mighty Meatball", Ingredients = "Meatballs and cheese", Cost = 40, InStock = "yes"},
-        //     new PizzaInfo { PizzaName = "Crab Apple", Ingredients = "Dungeness crab and apples", Cost = 35, InStock = "no"},
-        //     new PizzaInfo { PizzaName = "Forest Floor", Ingredients = "Mushrooms, rutabagas, and walnuts", Cost = 20, InStock = "yes"},
-        //     new PizzaInfo { PizzaName = "Don't At Me", Ingredients = "Pineapple, Canadian bacon, jalapeños", Cost = 25, InStock = "yes"},
-        //     new PizzaInfo { PizzaName = "Vanilla", Ingredients = "Sausage and pepperoni", Cost = 15, InStock = "no"},
-        //     new PizzaInfo { PizzaName = "Spice Coming At Ya", Ingredients = "Peppers, chili sauce, spicy andouille", Cost = 50, InStock = "yes"}
-        // };
         private readonly ILogger<PizzaDetailController> _logger;
         private readonly IPizzaRepo _pizzaRepo;
         private readonly IMapper _mapper;
--- frontend/PizzaClient.cs
+++ frontend/PizzaClient.cs
@@ -5,41 +5,42 @@ using Microsoft.Extensions.Logging;
 
 namespace frontend
 {
-   public class PizzaClient
-   {
-      private readonly JsonSerializerOptions options = new JsonSerializerOptions()
-      {
-         PropertyNameCaseInsensitive = true,
-         PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
-      };
+    public class PizzaClient
+    {
+        private readonly JsonSerializerOptions options = new JsonSerializerOptions()
+        {
+            PropertyNameCaseInsensitive = true,
+            PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+        };
 
-      private readonly HttpClient client;
-      private readonly ILogger<PizzaClient> _logger;
+        private readonly HttpClient client;
+        private readonly ILogger<PizzaClient> _logger;
 
-      public PizzaClient(HttpClient client, ILogger<PizzaClient> logger)
-      {
-         this.client = client;
-         this._logger = logger;
-      }
+        public PizzaClient(HttpClient client, ILogger<PizzaClient> logger)
+        {
+            this.client = client;
+            this._logger = logger;
+        }
 
-      public async Task<PizzaInfo[]> GetPizzasAsync()
-      {
-         try {
-            var responseMessage = await this.client.GetAsync("/pizzainfo");
-            
-            if(responseMessage!=null)
+        public async Task<PizzaInfo[]> GetPizzasAsync()
+        {
+            try
             {
-               var stream = await responseMessage.Content.ReadAsStreamAsync();
-               return await JsonSerializer.DeserializeAsync<PizzaInfo[]>(stream, options);
-            }
-         }
-         catch(HttpRequestException ex)
-         {
-            _logger.LogError(ex.Message);
-            throw;
-         }
-         return new PizzaInfo[] {};
+                var responseMessage = await this.client.GetAsync("api/pizzadetail");
 
-      }
-   }
+                if (responseMessage != null)
+                {
+                    var stream = await responseMessage.Content.ReadAsStreamAsync();
+                    return await JsonSerializer.DeserializeAsync<PizzaInfo[]>(stream, options);
+                }
+            }
+            catch (HttpRequestException ex)
+            {
+                _logger.LogError(ex.Message);
+                throw;
+            }
+
+            return new PizzaInfo[] { };
+        }
+    }
 }
