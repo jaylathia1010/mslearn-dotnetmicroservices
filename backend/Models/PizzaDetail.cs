63b76c0817bce9fa9aab2cc93022978b7a6e0345
Jay Lathia <jmlathia@gmail.com>
2022-10-03 17:41:08 +0530

    feat: added db context class

---
+++ backend/Models/PizzaDetail.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace backend.Models
+{
+    public class PizzaInfo
+    {
+        [Key]
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        public string PizzaName { get; set; }
+
+        [Required]
+        public string Ingredients { get; set; }
+
+        [Required]
+        public int Cost { get; set; }
+
+        [Required]
+        public string InStock { get; set; }
+    }
+}
--- backend/PizzaInfo.cs
+++ backend/PizzaInfo.cs
@@ -1,5 +1,3 @@
-using System;
-
 namespace backend
 {
     public class PizzaInfo
